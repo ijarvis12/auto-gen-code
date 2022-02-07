8a1d51dfcfe0251b79202e504b492d7739456b6f
Ian Jarvis <ijarvis12@gmail.com>
2022-02-07 09:49:15 -0600

    initial commit

---
+++ AutoGenPyCode.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+from multiprocessing import *
+
+def func(s,noHit):
+  try:
+    eval(s,None,None)
+    print('==============')
+    print(s)
+    print('==============')
+    print('\n\n\n')
+    noHit = False
+  except:
+    pass
+
+if __name__ == '__main__':
+
+  words = [chr(i) for i in range(33,127)]
+  words += ['\n','False','True','and','break','continue','elif','else','except','finally']
+  words += ['for','if','in','is','not','or','pass','try','while']
+  words += ['abs(','dict(','float(','int(','len(','list(','pow(','range(','str(','tuple(']
+
+  numList = [0]
+  n = len(numList)
+  
+  noHit = Manager().Value('b',True)
+  while noHit:
+    s = ""
+    numList[0] += 1
+    for k in range(len(numList)):
+      if numList[k] == n and k != (len(numList) - 1):
+        numList[k+1] += 1
+        numList[k] = 0
+      elif numList[k] == n and k == (len(numList) - 1):
+        numList.append(0)
+        numList[k] = 0
+    for l in numList:
+      s += (words[l]) + ' '
+
+    job = Process(target=func, args=(s,noHit,))
+    job.start()
+    job.join(15)
