91d06b9f2a7834945ad83c5ef1162865387954e2
Ian Jarvis <ijarvis12@gmail.com>
2022-03-23 10:11:34 -0500

    comments

--- AutoGenPyCode.py
+++ AutoGenPyCode.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+# function to evaluate auto gen code string in separate thread
 def func(s):
   try:
     eval(s,None,None)
@@ -9,22 +10,32 @@ def func(s):
     print('\n\n\n')
   except:
     pass
+  return
 
+# main function
 if __name__ == '__main__':
 
   from threading import Thread
 
+# list to hold all ASCII, keywords, and basic functions tokens
   words = [chr(i) for i in range(33,127)]
   words += ['\n','False','True','and','break','continue','elif','else:','except:','finally:']
   words += ['for','if','in','is','not','or','pass','try:','while']
   words += ['abs(','dict(','float(','int(','len(','list(','range(','str(','tuple(']
 
-  numList = [0]
+# length of python tokens var
   n = len(words)
-  
+
+# list to hold numeric value of auto gen code string (for iteration)
+  numList = [0]
+
+# main loop
   while True:
+#   auto gen code string var
     s = ""
+#   iterate list of numeric values
     numList[0] += 1
+#   carry over iteration if overflow
     for k in range(len(numList)):
       if numList[k] == n and k != (len(numList) - 1):
         numList[k+1] += 1
@@ -32,6 +43,7 @@ if __name__ == '__main__':
       elif numList[k] == n and k == (len(numList) - 1):
         numList.append(0)
         numList[k] = 0
+#   concatinate generate code string with some cleanup of syntax
     for i,l in enumerate(numList):
       if (i is not 0) and '(' in words[numList[i-1]]:
         s += words[l] + ') '
@@ -40,6 +52,7 @@ if __name__ == '__main__':
       else:
         s += words[l] + ' '
 
+#   create thread job for eval generated code and run (max 15 seconds)
     job = Thread(target=func, args=(s,))
     job.start()
     job.join(15)
