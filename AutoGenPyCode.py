f7cb87d1dec29d92ed72430c30951254a9fbea6a
Ian Jarvis <ijarvis12@gmail.com>
2022-03-23 11:28:15 -0500

    var renaming

--- AutoGenPyCode.py
+++ AutoGenPyCode.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 
 # function to evaluate auto gen code string in separate thread
-def func(s):
+def func(code):
   try:
-    eval(s,None,None)
+    eval(code,None,None)
     print('==============')
-    print(s)
+    print(code)
     print('==============')
     print('\n\n\n')
   except:
@@ -18,13 +18,13 @@ if __name__ == '__main__':
   from threading import Thread
 
 # list to hold all ASCII, keywords, and basic functions tokens
-  words = [chr(i) for i in range(33,127)]
-  words += ['\n','False','True','and','break','continue','elif','else:','except:','finally:']
-  words += ['for','if','in','is','not','or','pass','try:','while']
-  words += ['abs(','dict(','float(','int(','len(','list(','range(','str(','tuple(']
+  tokens = [chr(i) for i in range(33,127)]
+  tokens += ['\n','False','True','and','break','continue','elif','else:','except:','finally:']
+  tokens += ['for','if','in','is','not','or','pass','try:','while']
+  tokens += ['abs(','dict(','float(','int(','len(','list(','range(','str(','tuple(']
 
 # length of python tokens var
-  n = len(words)
+  lenTokens = len(tokens)
 
 # list to hold numeric value of auto gen code string (for iteration)
   numList = [0]
@@ -32,28 +32,29 @@ if __name__ == '__main__':
 # main loop
   while True:
 #   auto gen code string var
-    s = ""
+    code = ""
 #   iterate list of numeric values
     numList[0] += 1
+    lenNumList = len(numList)
 #   carry over iteration if overflow
-    for k in range(len(numList)):
-      if (numList[k] == n) and (k != (len(numList) - 1)):
-        numList[k+1] += 1
-        numList[k] = 0
-      elif (numList[k] == n) and (k == (len(numList) - 1)):
+    for idx in range(len(numList)):
+      if (numList[idx] == lenTokens) and (idx != (lenNumList - 1)):
+        numList[idx+1] += 1
+        numList[idx] = 0
+      elif (numList[idx] == lenTokens) and (idx == (lenNumList - 1)):
         numList.append(0)
-        numList[k] = 0
+        numList[dx] = 0
 #   concatinate generate code string with some cleanup of syntax
-    for i,l in enumerate(numList):
-      w = words[l]
-      if (i != 0) and ('(' in words[numList[i-1]]):
-        s += w + ') '
-      elif ':' in w:
-        s += w + '\n  '
+    for i,num in enumerate(numList):
+      token = tokens[num]
+      if (i != 0) and ('(' in tokens[numList[i-1]]):
+        code += token + ') '
+      elif ':' in token:
+        code += token + '\n  '
       else:
-        s += w + ' '
+        code += token + ' '
 
 #   create thread job for eval generated code and run (max 15 seconds)
-    job = Thread(target=func, args=(s,))
+    job = Thread(target=func, args=(code,))
     job.start()
     job.join(15)
