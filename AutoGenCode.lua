68af909a013706360e58569478022f4a853d8d8a
Ian Jarvis <ijarvis12@gmail.com>
2022-03-25 06:25:40 -0500

    extra token

--- AutoGenCode.lua
+++ AutoGenCode.lua
@@ -7,7 +7,7 @@ tokens = {'"','#','%','*','+','-','..','/','0','1','2','3','4','5','6','7','8','
           'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V',
           'W','X','Y','Z','^','a','b','c','d','e','f','g','h','i','j','h','i','j','k','l','m','n',
           'o','p','q','r','s','t','u','v','w','x','y','z','and','break','do','else','elseif','end',
-          'false','for','if','in','not','or','repeat','return','then','true','until','while'}
+          'false','for','if','in','not','or','repeat','then','true','until','while'}
 
 -- length of token table
 lenTokens = #tokens
